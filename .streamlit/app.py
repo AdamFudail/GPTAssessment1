000b2b186dcac34babef5357423f8e45754e25e5
adam18000878 <51939785+adam18000878@users.noreply.github.com>
2023-09-25 18:17:54 +0800

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -102,7 +102,7 @@ title_style = """
     text-align: center;
     text-transform: uppercase;
     margin-bottom: 30px;
-    margin-top: 80px;
+    margin-top: 5px;
 """
 st.markdown(
     f'<h1 style="{title_style}">ChemAI</h1>',
