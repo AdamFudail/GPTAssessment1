27581bdc4f5f3c8b03f6488341f9c6af3a94fce4
adam18000878 <51939785+adam18000878@users.noreply.github.com>
2023-09-25 18:24:03 +0800

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -101,7 +101,7 @@ title_style = """
     color: #FFFFFF;
     text-align: center;
     text-transform: uppercase;
-    margin-bottom: 5px;
+    margin-bottom: 30px;
     margin-top: 5px;
 """
 st.markdown(
